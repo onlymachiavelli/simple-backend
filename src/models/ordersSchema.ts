64d015ae9a14619fab637b4e5e873c5395bd89b6
onlymachiavelli <tmakaveli643@gmail.com>
2022-06-16 22:31:30 +0100

    orders schema

---
+++ src/models/ordersSchema.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+const ordersSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true, unique: true },
+    userID: { type: String, required: true },
+    products: { type: Array, required: true },
+    total: { type: Number, required: true },
+    status: { type: String, required: true },
+    createdAt: { type: Date, required: true },
+    updatedAt: { type: Date, required: true },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model("Orders", ordersSchema)
