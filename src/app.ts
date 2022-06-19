635041976fd4e602eefc51cc9f7705085cd21fe9
onlymachiavelli <tmakaveli643@gmail.com>
2022-06-19 23:39:57 +0100

    connecting to pgre server

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,27 @@
 import express from "express"
 import "dotenv/config"
-import connect from "./utils/mongoConnect"
+import appDataSource from "./utils/serverConnect"
 
 const app = express()
 const cors = require("cors")
 import { json } from "body-parser"
-import userRoute from "./routes/userRoute"
+//import userRoute from "./routes/userRoute"
 app.use(cors())
 app.use(json())
 const PORT = process.env.PORT || 3000
-
-app.use("/user", userRoute)
-connect()
+console.log(String(process.env.PASSWORD))
+//app.use("/user", userRoute)
+appDataSource
+  .initialize()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log("Server started on port 3000")
-    })
+    app
+      .listen(PORT, () => {
+        console.log(`server started at port ${PORT}`)
+      })
+      .on("error", (e) => {
+        console.log(e)
+      })
   })
   .catch((e) => {
-    console.log("connection error ! 'server !'" + e)
+    console.log(e)
   })
--- src/utils/serverConnect.ts
+++ src/utils/serverConnect.ts
@@ -1,3 +1,16 @@
+import { DataSource } from "typeorm"
+const appDataSource = new DataSource({
+  type: "postgres",
+  host: String(process.env.HOST),
+  port: Number(process.env.DBPORT),
+  username: String(process.env.USER) || "",
+  password: String(process.env.PASSWORD) || "",
+  database: String(process.env.DATABASE) || "",
+  entities: [__dirname + "/../models/*.ts"],
+})
+
+export default appDataSource
+
 /*
 import mongoose from "mongoose"
 
